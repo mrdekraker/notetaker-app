3f0d422d2f598cf092e3de05d7aaa28e66f73f40
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2022-10-26 05:56:06 -0500

    set up apiRoute

---
+++ routes/apiRoutes.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const fs = require('fs');
+
+// npm package to generate unique ids
+const { v4: uuidv4 } = require('uuid');
+
+// routing
+module.exports = (app) => {
+  // GET /api/notes should read db.json and return all saved notes as JSON
+  app.get(`/api/notes`, (req, res) => {
+    res.sendFile(path.join(__dirname, `../db/db.json`));
+  });
+
+  // POST receives notes to save on the request body, adds to db.json, then returns new note to client
+  app.post(`/api/notes`, (req, res) => {
+    // read db.json
+    const db = JSON.parse(
+      fs.readFileSync(path.join(__dirname, `../db/db.json`), 'utf8')
+    );
+
+    // add unique id to note
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uuidv4(),
+    };
+
+    // push new note to db.json
+    db.push(newNote);
+    fs.writeFileSync(`./db/db.json`, JSON.stringify(db));
+    res.json(db);
+  });
+};
